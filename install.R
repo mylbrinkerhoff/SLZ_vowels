85d6175258e31a65b4e989295108597a1427d59f
mlbrinke <mylbrinkerhoff@gmail.com>
2026-02-07 16:17:01 -0800

    Fix repo

--- install.R
+++ install.R
@@ -1,20 +1,2 @@
-#----------------------------------------------------------------------------------------
-# File: install.R
-# Project: SLZ_Vowels
-# Author: Mykel Brinkerhoff
-# Date: 2026-02-07 (Sa)
-# Description: Installs and loads renv into the Binder
-#              environment
-#
-# Usage:
-#   Rscript install.R
-#
-# Notes:
-#   - Ensure all required packages are installed.
-#   - Modify the script as needed for your specific dataset and analysis requirements.
-#----------------------------------------------------------------------------------------
-install.packages('renv', repos = c(CRAN = 'https://cloud.r-project.org'))
-
-library(renv)
-
+install.packages('renv')
 renv::restore()
